ecebc670289223f095bbbc58edfa66843690c6b4
danielmanu93 <67971334+danielmanu93@users.noreply.github.com>
2021-04-14 15:22:21 -0600

    Update gnn_controller.py

--- models/gnn_controller.py
+++ models/gnn_controller.py
@@ -11,7 +11,7 @@ state_space = {
     'activate_function': ["sigmoid", "tanh", "relu", "linear",
                           "softplus", "leaky_relu", "relu6", "elu"],
     'number_of_heads': [1, 2, 4, 6, 8, 12, 16, 20],
-    'hidden_units': [2, 4, 8, 16, 32, 64]
+    'hidden_units': [2, 4, 8, 16, 32, 64],
 }
 
 
